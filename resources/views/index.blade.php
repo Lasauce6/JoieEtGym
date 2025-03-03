b82380aee99a0bc32d44fcb8255e5149fd92774a
rbaticle <lasaucisse006@gmail.com>
2025-03-03 19:03:58 +0100

    restore photos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -186,9 +186,11 @@
             <div class="col-12 d-flex flex-wrap justify-content-center">
                 @foreach($animateurs as $animateur => $description)
                     <div class="p-3 text-center">
+                        <img src="{{ asset('/assets/images/animateurs/'.$animateur.'.png') }}" class="rounded-circle mb-2" alt="Photo de {{ $animateur }}" width="150" height="150">
                         <h5>{{ $animateur }}</h5>
                         <p class="text-muted">{!! $description !!}</p>
                     </div>
+
                 @endforeach
             </div>
         </div>
@@ -197,7 +199,9 @@
             <div class="col-12 d-flex flex-wrap justify-content-center">
                 @foreach($aquagym as $aq => $description)
                     <div class="p-3 text-center">
+                        <!--<img src="{{ asset('/assets/images/placeholder_profile.png') }}" class="rounded-circle mb-2" alt="Photo de {{ $aq }}" width="150" height="150">-->
                         <h5>{{ $aq }}</h5>
+                        <p class="text-muted">{!! $description !!}</p>
                     </div>
                 @endforeach
             </div>
@@ -208,6 +212,7 @@
             <div class="col-12 d-flex flex-wrap justify-content-center">
                 @foreach($bureau as $membre => $role)
                     <div class="p-3 text-center">
+                        <img src="{{ asset('/assets/images/placeholder_profile.png') }}" class="rounded-circle mb-2" alt="Photo de {{ $membre }}" width="150" height="150">
                         <h5>{{ $membre }}</h5>
                         <p class="text-muted">{{ $role }}</p>
                     </div>
